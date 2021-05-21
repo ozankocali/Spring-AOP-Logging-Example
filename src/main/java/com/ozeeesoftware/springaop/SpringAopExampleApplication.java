9681de25661f021df06503ba7a5fd6914d5eeb46
ozankocali <ozan.kocali@hotmail.com>
2021-05-21 17:56:39 +0300

    Initial commit model created

---
+++ src/main/java/com/ozeeesoftware/springaop/SpringAopExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ozeeesoftware.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozeeesoftware/springaop/SpringAopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozeeesoftware.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
